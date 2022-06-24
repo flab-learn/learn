da454d97953e295e926d7b555c01a1657281470f
cyh <drsggg>
2022-06-24 12:29:24 +0900

    1 commit test

--- src/test/java/study/flab/learn/LearnApplicationTests.java
+++ src/test/java/study/flab/learn/LearnApplicationTests.java
@@ -3,11 +3,11 @@ package study.flab.learn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LearnApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		System.out.println("1 pr test");
 	}
 
 }
