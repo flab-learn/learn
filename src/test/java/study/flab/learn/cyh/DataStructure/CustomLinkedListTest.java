12c738c54d708cee15c452095a5929e5a61d2adc
cyh789 <drsggg@naver.com>
2022-06-25 00:25:37 +0900

    feat : CustomArrayList 구현 v1.2
    CustomArrayList 직접구현하기 예제

--- src/test/java/study/flab/learn/cyh/DataStructure/CustomLinkedListTest.java
+++ src/test/java/study/flab/learn/cyh/DataStructure/CustomLinkedListTest.java
@@ -1,27 +1,24 @@
 package study.flab.learn.cyh.DataStructure;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CustomLinkedListTest {
 
-    private String[] params = new String[10];
     private CustomLinkedList customLinkedList;
-    private Collection javaLinkedList;
+    private List javaLinkedList;
+
 
     @BeforeEach
     void setUp() {
         javaLinkedList = new LinkedList();
         customLinkedList = new CustomLinkedList();
 
-        for (int i = 0; i < 10; i++) {
-            params[i] = i + "번";
-        }
     }
 
     @Test
@@ -131,6 +128,140 @@ class CustomLinkedListTest {
 
         assertThat(customLinkedList.contains("2")).isTrue();
         assertThat(customLinkedList.contains("3")).isFalse();
+    }
+
+    @Test
+    void containsAll() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        javaLinkedList.add("1");
+        javaLinkedList.add("2");
+
+        assertThat(customLinkedList.containsAll(javaLinkedList)).isTrue();
+    }
+
+    @Test
+    void isEmpty() {
+        assertThat(customLinkedList.isEmpty()).isTrue();
+
+        customLinkedList.add("1");
+        assertThat(customLinkedList.isEmpty()).isFalse();
+    }
+
+    @Test
+    void get() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+
+        assertThat(customLinkedList.get(0).toString()).isEqualTo("1");
+        assertThat(customLinkedList.get(1).toString()).isEqualTo("2");
+    }
+
+    @Test
+    void getTotalTest() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+
+        assertThat(customLinkedList.getFirst().toString()).isEqualTo("1");
+        assertThat(customLinkedList.getLast().toString()).isEqualTo("2");
+    }
+
+    @Test
+    void peekTotalTest() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+
+        assertThat(customLinkedList.peek()).isEqualTo("1");
+        assertThat(customLinkedList.peekFirst()).isEqualTo("1");
+        assertThat(customLinkedList.peekLast()).isEqualTo("2");
+    }
+
+    @Test
+    void remove() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        customLinkedList.add("3");
+//        System.out.println(customLinkedList.remove(1));
+        customLinkedList.remove(1);
+        assertThat(customLinkedList.toString()).isEqualTo("1,3");
+    }
+
+    @Test
+    void removeTotalTest() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        customLinkedList.add("3");
+        customLinkedList.add("4");
+        customLinkedList.add("5");
+
+//        System.out.println(customLinkedList.remove(0));
+//        assertThat(customLinkedList.toString()).isEqualTo("2,3,4,5");
+//        System.out.println(customLinkedList.removeFirst());
+//        assertThat(customLinkedList.toString()).isEqualTo("3,4,5");
+//        System.out.println(customLinkedList.removeLast());
+//        assertThat(customLinkedList.toString()).isEqualTo("3,4");
+//        System.out.println(customLinkedList.remove("4"));
+//        assertThat(customLinkedList.toString()).isEqualTo("3");
+
+        customLinkedList.remove(0);
+        assertThat(customLinkedList.toString()).isEqualTo("2,3,4,5");
+        customLinkedList.removeFirst();
+        assertThat(customLinkedList.toString()).isEqualTo("3,4,5");
+        customLinkedList.removeLast();
+        assertThat(customLinkedList.toString()).isEqualTo("3,4");
+        customLinkedList.remove("4");
+        assertThat(customLinkedList.toString()).isEqualTo("3");
+    }
+
+    @Test
+    void pop() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        assertThat(customLinkedList.pop()).isEqualTo("1");
+        assertThat(customLinkedList.toString()).isEqualTo("2");
+    }
+
+    @Test
+    void push() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        customLinkedList.push("3");
+        assertThat(customLinkedList.toString()).isEqualTo("3,1,2");
+    }
+
+    @Test
+    void removeAll() {
+        javaLinkedList.add("1");
+        javaLinkedList.add("2");
+
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        customLinkedList.add("3");
+
+        customLinkedList.removeAll(javaLinkedList);
+        assertThat(customLinkedList.toString()).isEqualTo("3");
+    }
+
+    @Test
+    void set() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+        customLinkedList.add("3");
+
+        customLinkedList.set(0, "4");
+        assertThat(customLinkedList.toString()).isEqualTo("4,2,3");
+    }
+
+    @Test
+    void subList() {
+        customLinkedList.add("1");
+        customLinkedList.add("2");
+
+        javaLinkedList = customLinkedList.subList(0, 2);
+        Object[] sArr = javaLinkedList.toArray();
+        for (int i = 0; i < sArr.length; i++) {
+            assertThat(sArr[i].toString()).isEqualTo(customLinkedList.get(i).toString());
+        }
     }
 
 }
