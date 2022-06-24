f531ae0aa063cfbae245cf5a56ebd6d11dc547bf
cyh789 <drsggg@naver.com>
2022-06-24 11:18:44 +0900

    Add files via upload
    create project

---
+++ src/test/java/study/flab/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package study.flab.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
